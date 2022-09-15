a0832773b741462595dff4546c29fe8cf4154b90
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-09-15 08:33:57 +0530

    Add files via upload

---
+++ CDL/lab3/exinput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=5,b=10;
+    int s=b+a;
+    int d=b-a;
+    int p=b*a;
+    int q=b/a;
+    printf("Sum = %d\nDifference = %d\nProduct = %d\nQuotient = %d\n",s,d,p,q);
+    return 0;
+}
---
+++ CDL/lab3/space_output.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a=3,b=2e+3;;
+ char c;
+ float k;
+ if(a<b||a<0)
+ printf("yes");
+ else if(a==b)
+ printf("equal");
+ else
+ printf("no");
+
+ return 0;
+}
+
