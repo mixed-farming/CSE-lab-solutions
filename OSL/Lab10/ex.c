5a345b4bb4201bb5de2b951bb6156c052a3dadea
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-11-19 07:41:30 +0530

    Add files via upload

---
+++ OSL/Lab10/ex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numSecteur=10;
+    char sectuer[1024];
+    FILE *disqueF=fopen("/dev/block","r");
+    fseek(disqueF,numSecteur*512,SEEK_SET);
+    fread(sectuer,1024,1,disqueF);
+    for(int i=0;i<sizeof(sectuer);i++)
+    {
+        printf("%x",sectuer[i]);
+        if((i+1)%16==0)
+        printf("\n");
+    }
+    fclose(disqueF);
+    return 0;
+}
