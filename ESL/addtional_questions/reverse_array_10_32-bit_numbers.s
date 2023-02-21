37e98ba26de2dc3b0143ff11735ff4ec03b1cd09
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-02-21 19:52:27 +0530

    Create reverse_array_10_32-bit_numbers.s

---
+++ ESL/addtional_questions/reverse_array_10_32-bit_numbers.s
@@ -0,0 +1,43 @@
+AREA RESET,DATA,READONLY
+    EXPORT __Vectors
+ 
+__Vectors
+    DCD 0x10001000 ; stack pointer when stack is emtpy
+    DCD Reset_Handler ; reset vector
+    ALIGN
+    AREA mycode,CODE,READONLY
+    ENTRY
+    EXPORT Reset_Handler
+ 
+Reset_Handler
+    MOV R3,#10
+    LDR R0,=SRC
+    LDR R1,=DST
+    
+loop1
+    LDR R2,[R0],#4
+    STR R2,[R1],#4
+    SUBS R3,#1
+    BNE loop1
+    
+    MOV R5,#5
+    LDR R0,=DST
+    LDR R1,=DST
+    ADD R1,#36
+    
+loop2
+    LDR R2,[R0]
+    LDR R3,[R1]
+    STR R3,[R0],#4
+    STR R2,[R1],#-4
+    SUBS R5,#1
+    BNE loop2
+    
+STOP 
+    B STOP
+    
+    AREA mydata1,DATA,READONLY
+SRC DCD 0x10,0x9,0x8,0x7,0x6,0x5,0x4,0x3,0x2,0x1
+    AREA mydata2,DATA,READWRITE
+DST DCD 0
+    END
