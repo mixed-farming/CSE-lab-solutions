4ae3fe802daead25db7848c0151669c873704b5a
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-03-17 00:23:11 +0530

    Update l4extra2.c

--- PCAPL/week4/l4extra2.c
+++ PCAPL/week4/l4extra2.c
@@ -4,7 +4,6 @@
 int main(int argc, char *argv[])
 {
     int rank, size;
-    MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
