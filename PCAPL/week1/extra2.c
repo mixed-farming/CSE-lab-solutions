a80563b4d06cb8f33a7adee8f7dc35e4fa789f0f
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-03-16 19:49:54 +0530

    Update extra2.c

--- PCAPL/week1/extra2.c
+++ PCAPL/week1/extra2.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    // set number of process to 9 during mpi run-time
+    // set number of process to 2 during mpi run-time
     if (size != 2) {
     printf("This program must be run with exactly 2 processes.\n");
     MPI_Finalize();
