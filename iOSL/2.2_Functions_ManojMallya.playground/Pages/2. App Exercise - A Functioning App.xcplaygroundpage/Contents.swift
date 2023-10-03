9d22173be30456545859cf14187987ca163b49fb
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-10-03 16:01:22 +0530

    Add files via upload

---
+++ iOSL/2.2_Functions_ManojMallya.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+/*:
+## App Exercise - A Functioning App
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ As you may have guessed, functions are key to making your app work. For example, in every exercise dealing with step count until now, you have simply assigned a number of steps to a `steps` variable. This isn't very realistic seeing as the number of steps you take increments one at a time and continues changing throughout the day.
+ 
+ A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
+ */
+
+func incrementSteps(step: Int){
+    steps=steps+1
+    print(steps)
+}
+
+var steps = 0
+incrementSteps(step: steps)
+incrementSteps(step: steps)
+
+//:  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout.
+let goal = 10000 
+
+func progressUpdate(target: Int)
+{
+    if target < goal/10{
+        print("You're off to a good start")
+    }
+    else if target < goal/2{
+        print("You're almost halfway there!")
+    }
+    else if target < (goal*9)/10{
+        print("You're over halfway there!")
+    }
+    else if target < goal{
+        print("You're almost there!")
+    }
+    else
+    {
+        print("You beat your goal!")
+    }
+}
+
+progressUpdate(target: steps)
+/*:
+[Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
+ */
