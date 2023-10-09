b4447e501a16742da6e96e01287cad063f55135e
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-10-09 20:52:04 +0530

    Add files via upload

---
+++ iOSL/2.6_Loops_ManojMallya.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+## Exercise - For-In Loops
+ 
+ Create a for-in loop that loops through values 1 to 100, and prints each of the values.
+ */
+for index in 1...100 {
+    print(index)
+}
+
+//:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+for (index, letters) in alphabet.enumerated() {
+    print("\(index): \(letters)")
+}
+
+
+//:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
+let states: [String: String] = ["Karnataka": "Bangalore", "Maharastra": "Mumbai", "Rajasthan": "Jaipur"]
+
+for (stateName, stateCaptial) in states {
+    print("\(stateCaptial) is the captial of \(stateName).")
+}
+
+/*:
+page 1 of 6  |  [Next: App Exercise - Movements](@next)
+ */
---
+++ iOSL/2.6_Loops_ManojMallya.playground/Pages/3. Exercise - While Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+/*:
+## Exercise - While Loops
+ 
+ Create a while loop that simulates rolling a 6-sided dice repeatedly until a 1 is rolled. After each roll, print the value. (Hint: use `Int.random(in: 1...6)` to generate a random number between 1 and 6).
+ */
+var roll = 0
+
+while (roll != 1) {
+    roll = Int(arc4random_uniform(6) + 1)
+    print(roll)
+}
+/*:
+[Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Running Cadence](@next)
+ */
