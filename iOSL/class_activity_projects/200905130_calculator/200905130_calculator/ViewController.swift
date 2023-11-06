30299584c42e694034bcffebef0d931d77cb4ef7
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-11-06 14:00:48 +0530

    Add files via upload

---
+++ iOSL/class_activity_projects/200905130_calculator/200905130_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  200905130_calculator
+//
+//  Created by Student on 16/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        print(view.subviews)
+    }
+
+
+}
+
---
+++ iOSL/class_activity_projects/200905130_calculator/200905130_calculatorUITests/_00905130_calculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _00905130_calculatorUITestsLaunchTests.swift
+//  200905130_calculatorUITests
+//
+//  Created by Student on 16/10/23.
+//
+
+import XCTest
+
+final class _00905130_calculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
