19c41afd7084cd442d1b4b215d29feeb81e75f31
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-11-06 14:02:30 +0530

    Add files via upload

---
+++ iOSL/class_activity_projects/classExerciseProject/classExerciseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classExerciseProject
+//
+//  Created by Student on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
