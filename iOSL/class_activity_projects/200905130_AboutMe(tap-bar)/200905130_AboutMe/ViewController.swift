874f3c54bd5f43ba334581a5251eecaf30358685
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-11-06 16:08:15 +0530

    Add files via upload

---
+++ iOSL/class_activity_projects/200905130_AboutMe(tap-bar)/200905130_AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  200905130_AboutMe
+//
+//  Created by Student on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
