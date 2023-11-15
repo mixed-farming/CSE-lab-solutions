ac613a27d2d98174c3673fb2e231c9584ef77ca5
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2023-11-15 15:01:20 +0530

    Add files via upload

---
+++ iOSL/class_activity_projects/programmatic segues/programmatic segues/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  programmatic segues
+//
+//  Created by Student on 30/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var segueSwitch: UISwitch!
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+}
+
